c421747b260248381448af6b7b765beea5e92120
DJ <DJ@email.com>
2020-02-04 20:17:30 +0800

    专门处理用户信息 vuex信息交互

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import * as auth from '@/utils/auth'
 Vue.use(Vuex)
-
+// vuex 和缓存数据的同步
 export default new Vuex.Store({
+  // 放置公共数据
+  // 初始化时  直接将用户信息给公共状态
   state: {
+    user: auth.getUser()// 从缓存中读取
   },
+  // state修改必须通过mutations
   mutations: {
+    updateUser (state, payload) {
+      state.user = payload.user // 更新数据
+      auth.setUser(payload.user)// 将数据同步设置在本地存储中
+    },
+    // 情况user
+    clearUser (state) {
+      state.user = {}
+      auth.delUser()// 将缓存中的数据清空
+    }
   },
   actions: {
   },
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,11 +2,13 @@
 const USER_TOKEN = 'hm-yidong-token' // key  专门用来存储用户信息
 // 存储用户信息
 export function setUser (user) {
+  // localStorage 存储的是字符串  要把对象转成字符串
   localStorage.setItem(USER_TOKEN, JSON.stringify(user))
 }
 // 读取用户信息
 export function getUser () {
-  // 短路表达式  前边true 后边不执行  前边false 后边执行
+  // 导出的是字符串 要转成对象
+  // 短路表达式  前边true 后边不执行  前边false 后边执行  空字符串会报错
   return JSON.parse(localStorage.getItem(USER_TOKEN) || '{}')
 }
 // 删除用户信息
