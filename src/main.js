360b78c62f5ce73459df486c908f9a26213fdc5a
DJ <DJ@email.com>
2020-02-04 22:33:47 +0800

    提交主体颜色修改

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './permission'// 经过权限处理之后的router
 import store from './store'
 import Vant from 'vant'
-import 'vant/lib/index.css'
+import 'vant/lib/index.less'
 import '@/styles/index.less'// 引入全局的样式 覆盖vant
 import 'amfe-flexible' // 引入自动适配插件
 Vue.use(Vant) // 全局注册  在任意位置可以使用Vant组件库 实际上调用了 Vant 的 install方法
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="container">
-    <van-nav-bar title="登录"  left-arrow @click-left="$router.back()"></van-nav-bar>
+    <van-nav-bar title="登录" left-arrow @click-left="$router.back()"></van-nav-bar>
+    <!-- 手机号 验证码 登录按钮 -->
+    <van-cell-group>
+      <van-field label="手机号" placeholder="请输入手机号码"></van-field>
+      <van-field label="验证码" placeholder="请输入验证码">
+        <!-- slot指定给哪个坑提案内容 -->
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+      <div class="btn-box">
+        <van-button type="info" block round size="small">登录</van-button>
+      </div>
+    </van-cell-group>
   </div>
 </template>
 
@@ -11,4 +22,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.btn-box{
+  padding: 10px;
+}
 </style>
