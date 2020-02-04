3bbc735237a263d6a6d3ee46604dd27c80ae2fc9
DJ <DJ@email.com>
2020-02-04 14:30:25 +0800

    导入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant) // 全局注册  在任意位置可以使用Vant组件库 实际上调用了 Vant 的 install方法
 Vue.config.productionTip = false
 
 new Vue({
